c12154fb0699063aca1a880e8bddc08bbf019da2
charnelledev <kengnemaiva54@gmail.com>
2025-01-09 15:20:08 +0100

    first commit

---
+++ selecteurs-js/script.js
@@ -0,0 +1,48 @@
+let nombre = 2;
+console.log(nombre);
+
+// document.querySelector est une methode js qui permet de selectionner les elements dans le dom d'une page web
+
+// let titre = document.querySelector("h1"); 
+// let btn01 = document.querySelector(".btn-1");
+// let btn02 = document.querySelector(".btn-2");
+// let text = document.querySelector("#text");
+
+// console.log(titre);
+// console.log(btn01);
+// console.log(btn02);
+// console.log(text);
+
+
+
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.querySelector("btn-2");
+const questioncontent = document.querySelector(".click-btn")
+console.log(questioncontent);
+
+// console.log(btn1);
+// console.log(btn2);
+
+// btn1.addEventListener("evenement",fonction)
+
+// btn1.addEventListener("evenement",fonction)
+
+ function addition(a,b){
+    
+    return a + b;
+}
+let somme = addition (5,9) //appel de fonction 
+console.log(somme);
+
+ const add = function (a1,b1) {
+    return a1 + b1;
+}
+
+
+// foncton flechee
+ const add2 = (a2,b2)=> {
+    return  a2 + b2;
+    
+}
+ const add3 = ()=> som4 = a1 + b;
+  
